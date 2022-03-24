48b7e067e56d7b208950595f14d428ca21f860e6
tomernahum <101682824+tomernahum@users.noreply.github.com>
2022-03-24 00:07:01 -0400

    Added some settings + started reworking cut

---
+++ AutoEditorV0.3/open_in_terminal.bat
@@ -0,0 +1,2 @@
+
+start cmd /k cd C:\Users\Tomer\Documents\GitHub\auto-video-cutter\AutoEditorV0.3
--- AutoEditorV0.3/record.py
+++ AutoEditorV0.3/record.py
@@ -8,15 +8,17 @@
 #      - would this entail replacing the print statements basically? Maybe even replacing keyboard shudder
 # todo idea: should it count the projected length of the output video as you're going? Yes methinks - that could get complicated with retake a
 #      - 
-#bug/todo: Indent from \t between time and Accepted/Rejected/etc is too short
+#bug/todo: Indent from \t between time and Accepted/Rejected/etc is too short (maybe)
+#bug: pressing hotkeys in another program does activate but also can make error sound or trigger that program's hotkeys
 
 import datetime
 import time
+from urllib.parse import parse_qs
 import keyboard
 
 
 segments_done = 0 #Q/N: this is apparently bad form but I don't think I can pass / return it (there could be a way actually)with the keyboard module I'm using... So I will put the global statement at the top so you know what modifies it...
-edited_vid_length = 0 #WIP
+edited_vid_length = 0 #post-cuts_vid_length, final_vid_length, projected_final_length
 
 
 
@@ -26,6 +28,7 @@ def start_record_timestamps_mode():
     segments_done = 0
 
     hotkeys_dict = get_hotkeys_dict()
+    settings_dict = get_settings_dict()
     output_file = open(ask_for_output_file_name(),"w")
     
     #write file header
@@ -50,7 +53,7 @@ def start_record_timestamps_mode():
     print("Started")
 
     #add hotkeys with keyboard module
-    add_hotkey_detection(hotkeys_dict, start_time, output_file)
+    add_hotkey_detection(hotkeys_dict, start_time, output_file, settings_dict)
     #each hotkey executes a function basically, (those being mark_cut() and WIPmark_effect())
     
     #wait till stop hotkey is pressed
@@ -62,23 +65,22 @@ def start_record_timestamps_mode():
     
     #maybe todo "would you like to edit it now?"
 
-def add_hotkey_detection(hotkeys_dict, start_time, output_file):
+def add_hotkey_detection(hotkeys_dict, start_time, output_file, settings_dict):
     #keyboard.add_hotkey(hotkeys_dict["accept"]) reject, 3rd
     
     for hotkey_function_label, literal_hotkey_str in hotkeys_dict.items():
         if  hotkey_function_label in {"start recording","end recording"}: continue  #is this inneficient?
         
         if hotkey_function_label in {"accept","reject","retake accepted"}:
-            keyboard.add_hotkey(literal_hotkey_str, mark_cut, args=[hotkey_function_label, start_time, output_file])
+            keyboard.add_hotkey(literal_hotkey_str, mark_cut, args=[hotkey_function_label, start_time, output_file, settings_dict])
             # makes it so that if you press the hotkey while code is running
             # it runs the function mark_cut w/ the label as an input
         else:
             keyboard.add_hotkey(literal_hotkey_str, print, args=["temp"])
             #this will be custom effects markers&doers hotkeys
 
-
-def mark_cut(label, start_time, output_file):
-    #prints and write to file cut + prints and tracks segments_done
+#prints and writes to file timestamp & cut label + prints and tracks segments_done & edited_vid_length
+def mark_cut(label, start_time, output_file, settings_dict):
     global segments_done
 
     time_elapsed = get_time_elapsed_str(start_time)
@@ -86,36 +88,42 @@ def mark_cut(label, start_time, output_file):
     # time_elapsed could be moved to the outside and just passed in 
     # (still found with same function), should it be generally?)"""
     
+    #Determine past_tense_label and modify segments_done and edited_vid_length
+    #past_tense_label, segments_done_change = find_some_stuff(label)
 
-    #should this be split into 3 functions? They all essentially do the same thing
-    if label == "accept":
+    past_tense_label = ""
+    segments_done = segments_done  #very important line of code  # could change to segments_done_change and add outside of loop
+    if label == "accept": 
+        past_tense_label += "Accepted"
         segments_done += 1
-        print(time_elapsed + "\tAccepted" 
-            + "\t%2d segments accepted, now recording #%2d" % (segments_done, segments_done+1))
-        output_file.write(time_elapsed + "\tAccepted\n")
-    
-    elif label == "reject":
+    elif label == "reject": 
+        past_tense_label += "Rejected"
         #segments_done += 0
-        print(time_elapsed + "\tRejected" 
-            + "\tre-recording segment #%2d" % (segments_done+1))
-        output_file.write(time_elapsed + "\tRejected\n")
-    
-    elif label == "retake accepted":
+    elif label == "retake accepted": 
+        past_tense_label += "Retake A."
         segments_done += -1
-        print(time_elapsed + "\tRetake A." 
-            + "\tre-recording the previously accepted segment #%2d" % (segments_done+1))
-        output_file.write(time_elapsed + "\tRetake A.\n")
-
-    elif label == "end": #called from after keyboard.wait not from keyboard.addhotkey
-        print(time_elapsed + "\tEnded    " 
-            + "\tEnding Segment Auto-Discarded, %2d segments accepted in total," % segments_done)
-        output_file.write(time_elapsed + "\tEnded\n")
+    elif label == "end": 
+        past_tense_label += "Ended"
+        #segments_done += 0
     
-    # this should:
-    # - have the timestamp (time elapsed)
-    # - update "segments done" 
-    # - write to file
-    # - print to terminal
+    #build the to-print string based on settings
+    to_print_str = ""
+    to_print_str += time_elapsed
+    to_print_str += "\t" + past_tense_label
+    if settings_dict["Include completed segment count (yes/no)"] == "yes": 
+        to_print_str += "\t" + segments_done
+    if settings_dict["Include projected final time (yes/no)"] == "yes":
+        pass #WIP
+    
+    #build the to-write (in file) string
+    to_write_str = time_elapsed + "\t" + past_tense_label + "\n"
+
+    #perform the final outputs
+    print(to_print_str)
+    output_file.write(to_write_str)
+    
+    
+    
 
 
 def get_time_elapsed_str(start_time):
@@ -135,6 +143,13 @@ def get_hotkeys_dict():
             "accept":"alt+q", "reject":"alt+w", "retake accepted":"alt+e",
             "test-effect":"alt+z"}
     #Todo: Settings file with hotkeys customization + infinite custom name effect hotkeys
+
+def get_settings_dict():
+    #In file will be under Record Mode Settings
+    settings_dict = {"Include completed segment count (yes/no)" : "yes",
+                    "Include projected final time (yes/no)":"no", #WIP
+                    "Retake Accepted Limit (1/2/no)" : "no"}
+    return settings_dict
 
 def ask_for_output_file_name(): 
     output_file = input("enter the name of the file to write to (no extention): ")+".txt"
