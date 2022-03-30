f01077d944576ce1170b69c117d37d1b57575429
tomernahum <101682824+tomernahum@users.noreply.github.com>
2022-03-30 19:09:11 -0400

    worked on proccess to create segments list out of timestamps file

--- Proccess Mode/v1/process.py
+++ Proccess Mode/v1/process.py
@@ -2,8 +2,7 @@ from moviepy.editor import *
 from moviepy.video import fx
 
 class Segment:
-    def __init__(self, t_start, t_end, parent_VFC, effects_list):
-        self.raw_start = t_start
+    def __init__(self, t_start, t_end, parent_VFC, effects_list):  #Q/N, optimization: parent_VFC ideally would just be a pointer to main VFC, not a copy, so i guess it should be a global variable
         self.raw_end = t_end
         self.VFC = parent_VFC.subclip(t_start, t_end)
         self.effects_list = effects_list  #effects will be their own objects
@@ -13,14 +12,19 @@ class Segment:
     def get_VFC(self):
         return self.VFC
     
-    def apply_effect_to_self(self, effect_object): #apply effects on to VFC
+    def add_effect(self, effect):
+        self.effects_list.append(effect)
+    def remove_effect(self, effect):
+        self.effects_list.remove(effect)
+
+    def apply_effect_to_own_VFC(self, effect_object): #apply effects on to VFC
         output = effect_object.apply_effect(self.VFC)
         self.VFC = output
     
     def apply_all_effects(self):
         #WIP: may have to add priorities or something
         for effect_object in self.effects_list:
-            self.apply_effect_to_self(effect_object)
+            self.apply_effect_to_own_VFC(effect_object)
 
     
 class Effect:
@@ -40,13 +44,15 @@ class Greyscale(Effect):
 
 
 
-
-
 def start_process_mode():
-    video_file_name = "test_vid.mkv"
+    #open timestamps file reader & Master Video File Clip
+    video_file_name, timestamps_file = get_file_names_from_user()
     main_VFC = VideoFileClip(video_file_name)
+    timestamps_file = open(timestamps_file, 'r')
     
-    
+    segments_list = get_segments_list(timestamps_file, main_VFC)
+
+"""
     segment = Segment( 10, 20, main_VFC, effects_list=[Greyscale(100)])
     segment.apply_all_effects()
     #segment.get_VFC().resize(width=500).preview()
@@ -55,6 +61,34 @@ def start_process_mode():
     output.write_videofile("test.mp4")
 
     print("Done!")
+"""
+
+
+def get_file_names_from_user():
+    return "test_vid.mkv", "realtest.txt"
+
+def get_segments_list(timestamps_file, main_VFC):
+    segments_list = []
+    
+    #make a .split tuples list of just cut action timestamps (accept/reject/retake/maybe end)
+    cut_action_timestamps_list = []  #[0] is timestamp [1] is label
+    for line in timestamps_file.readlines()[1:-1]:  #last bit makes it skip 1st & last lines
+        split_line = line.strip().split("\t")
+        if split_line[1] in {"Accepted","Rejected","Retake A.", "Ended"}:
+            cut_action_timestamps_list.append(split_line)
+    
+    #put cut & none segments in segments_list
+    last_timestamp = 0
+    for timestamp, label in cut_action_timestamps_list:
+        print(timestamp, label)
+        #to be continued
+        pass
+
+    
+    
+    
+    
+
 
 if __name__ == "__main__":
     start_process_mode()
